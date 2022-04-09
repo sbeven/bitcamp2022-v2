65c4a6cecc6a12494d21952603eaf99ae1222d95
alemtani <alex.m.tanimoto@gmail.com>
2022-04-09 19:06:27 -0400

    Add cut logic to end screen and restart

--- MoveNet_Refined.py
+++ MoveNet_Refined.py
@@ -12,7 +12,8 @@ import cv2
 import time
 import random
 
-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 EDGES = {
     (0, 1): 'm',
@@ -73,7 +74,10 @@ paths = ["Mask1.png",
         "Mask9.png",
         "Mask10.png",
         "Mask11.png",
-        "Mask12.png"]
+        "Mask12.png",
+        "Mask13.png",
+        "Mask14.png",
+        "Mask15.png"]
 name = "Hole in the Wall"
 videoWidth = 1000
 videoHeight = 750
@@ -89,117 +93,22 @@ score = 0
 rounds = 0
 end = False
 skipEvent = False
-input
+while not end: # Repeat indefinitely as long not "q"
+    fails = 0
+    score = 0
+    rounds = 0
+    skipEvent = False
+    cut = False # Stop the game and show exit screen
+    restart = False # Allow user to restart in-game
 
-#user calibration
-start = time.time()
-finish = time.time()
-skipEvent = False
-mask_path = paths[0] #replace with calibration mask
-mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
-while not end and not skipEvent:
-
-    collision = False
-    ret, frame = cap.read()
-
-    # Reshape image
-    img = frame.copy()
-    img = tf.image.resize_with_pad(np.expand_dims(img, axis=0), 192, 192)
-    input_image = tf.cast(img, dtype=tf.float32)
-    # Setup input and output
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-
-    # Make predictions
-    interpreter.set_tensor(input_details[0]['index'], np.array(input_image))
-    interpreter.invoke()
-    keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
-
-    # resize and render mask
-
-    frame = cv2.resize(frame, dsize=(videoWidth, videoHeight), interpolation=cv2.INTER_CUBIC)
-    mask = cv2.resize(mask, dsize=(videoWidth, videoHeight), interpolation=cv2.INTER_CUBIC)
-
-    # change dtype of mask
-    mask = mask.astype('uint8')
-
-    frame = np.concatenate((frame, np.full((videoHeight, videoWidth, 1), 255, dtype=np.uint8)), axis=2)
-    frame = cv2.addWeighted(frame, 1, mask, 0.5, 0)
-
-    y, x, c = frame.shape
-    shaped = np.squeeze(np.multiply(keypoints_with_scores, [y, x, 1]))
-    # collisions
-    for i in range(0, len(shaped)):
-        xpos = shaped[i][0]
-        ypos = shaped[i][1]
-        confidence = shaped[i][2]
-        if xpos < y and ypos < x and confidence > 0.4:
-            if mask[int(xpos), int(ypos), 3] > 0:
-                colors[i] = (0, 0, 0)
-                collision = True;
-            else:
-                colors[i] = (255, 255, 255)
-    # Rendering
-    draw_connections(frame, keypoints_with_scores, EDGES, 0.4)
-    draw_keypoints(frame, shaped, 0.4, colors)
-
-    frame = cv2.flip(frame, 1)
-    # put the timer on the screen
-    if collision:
-        start = time.time()
-        frame = cv2.putText(frame, "Calibrate body", (15, 50), \
-                            fontface, fontscale, fontcolor, thickness=3)
-        # red dot to show that there's a collision
-        frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0,0,255), thickness=-1)
-    else:
-        frame = cv2.putText(frame, "Hold position: " + str(3 - int(finish - start)), (15, 50), \
-                        fontface, fontscale, fontcolor, thickness=3)
-        frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0, 255, 0), thickness=-1)
-    cv2.imshow(name, frame)
-    input = cv2.waitKey(10)
-    if input == ord('q'):
-        end = True
-    if input == ord('s'):
-        skipEvent = True
-    if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
-        end = True
-    finish = time.time()
-    if finish - start > 3:
-        skipEvent = True
-skipEvent = False
-# game start text
-start = time.time()
-finish = time.time()
-while finish - start < 3 and not end and not skipEvent:
-    frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (255, 255, 255), thickness=-1)
-    frame = cv2.putText(frame, "Game will start in " + \
-                        str(3 - int(finish - start)), (int(videoWidth / 2) - 330,int(videoHeight / 2)), \
-                        fontface, fontscale, fontcolor, thickness=3)
-    cv2.imshow(name, frame)
-    finish = time.time()
-    input = cv2.waitKey(10)
-    if input == ord('q'):
-        end = True
-    if input == ord('s'):
-        skipEvent = True
-    if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
-        end = True
-
-random.shuffle(paths)
-used = []
-skipEvent = False
-# game start screen
-while fails < 3 and not end:
-
+    #user calibration
     start = time.time()
     finish = time.time()
     skipEvent = False
-    mask_path = paths.pop()
+    mask_path = paths[0] #replace with calibration mask
     mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
-    used.append(mask_path)
+    while not end and not skipEvent and not cut and not restart:
 
-    #game loop
-    while finish - start < 5 and not end and not skipEvent:
         collision = False
         ret, frame = cap.read()
 
@@ -227,7 +136,6 @@ while fails < 3 and not end:
         frame = np.concatenate((frame, np.full((videoHeight, videoWidth, 1), 255, dtype=np.uint8)), axis=2)
         frame = cv2.addWeighted(frame, 1, mask, 0.5, 0)
 
-
         y, x, c = frame.shape
         shaped = np.squeeze(np.multiply(keypoints_with_scores, [y, x, 1]))
         # collisions
@@ -246,13 +154,16 @@ while fails < 3 and not end:
         draw_keypoints(frame, shaped, 0.4, colors)
 
         frame = cv2.flip(frame, 1)
-        #put the timer on the screen
-        frame = cv2.putText(frame, str(5 - int(finish - start)), (15, 50), \
-                            fontface, fontscale, fontcolor, thickness=3)
+        # put the timer on the screen
         if collision:
-            # show collision circle
-            frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0, 0, 255), thickness=-1)
+            start = time.time()
+            frame = cv2.putText(frame, "Calibrate body", (15, 50), \
+                                fontface, fontscale, fontcolor, thickness=3)
+            # red dot to show that there's a collision
+            frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0,0,255), thickness=-1)
         else:
+            frame = cv2.putText(frame, "Hold position: " + str(3 - int(finish - start)), (15, 50), \
+                            fontface, fontscale, fontcolor, thickness=3)
             frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0, 255, 0), thickness=-1)
         cv2.imshow(name, frame)
         input = cv2.waitKey(10)
@@ -260,38 +171,24 @@ while fails < 3 and not end:
             end = True
         if input == ord('s'):
             skipEvent = True
-        if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
-            end = True
+        if input == ord('c'):
+            cut = True
+        if input == ord('r'):
+            restart = True
+        # if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
+        #     end = True
         finish = time.time()
+        if finish - start > 3:
+            skipEvent = True
     skipEvent = False
-
-    #display a fail/success screen
+    # game start text
     start = time.time()
     finish = time.time()
-    if collision:
-        fails += 1
-    else:
-        score += 1
-    rounds += 1
-    while finish - start < 3 and not end and not skipEvent:
-        if fails == 3:
-            frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 0, 255), thickness=-1)
-            frame = cv2.putText(frame, "Game Over", (int(videoWidth / 2) - 180,int(videoHeight / 2)), \
-                                fontface, fontscale, fontcolor, thickness=3)
-            frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
-                                fontface, fontscale, fontcolor, thickness=3)
-        elif collision:
-            frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 0,255), thickness = -1)
-            frame = cv2.putText(frame, "Fail", (int(videoWidth / 2) - 60,int(videoHeight / 2)), \
-                                fontface, fontscale, fontcolor, thickness=3)
-            frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
-                                fontface, fontscale, fontcolor, thickness=3)
-        else:
-            frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 255, 0), thickness=-1)
-            frame = cv2.putText(frame, "Pass", (int(videoWidth / 2) - 80,int(videoHeight / 2)), \
-                                fontface, fontscale, fontcolor, thickness=3)
-            frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
-                                fontface, fontscale, fontcolor, thickness=3)
+    while finish - start < 3 and not end and not skipEvent and not cut and not restart:
+        frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (255, 255, 255), thickness=-1)
+        frame = cv2.putText(frame, "Game will start in " + \
+                            str(3 - int(finish - start)), (int(videoWidth / 2) - 330,int(videoHeight / 2)), \
+                            fontface, fontscale, fontcolor, thickness=3)
         cv2.imshow(name, frame)
         finish = time.time()
         input = cv2.waitKey(10)
@@ -299,30 +196,169 @@ while fails < 3 and not end:
             end = True
         if input == ord('s'):
             skipEvent = True
-        if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
+        if input == ord('c'):
+            cut = True
+        if input == ord('r'):
+            restart = True
+        # if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
+        #     end = True
+
+    random.shuffle(paths)
+    used = []
+    skipEvent = False
+    # game start screen
+    while fails < 3 and not end and not cut and not restart:
+
+        start = time.time()
+        finish = time.time()
+        skipEvent = False
+        mask_path = paths.pop()
+        mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
+        used.append(mask_path)
+
+        #game loop
+        while finish - start < 5 and not end and not skipEvent and not cut and not restart:
+            collision = False
+            ret, frame = cap.read()
+
+            # Reshape image
+            img = frame.copy()
+            img = tf.image.resize_with_pad(np.expand_dims(img, axis=0), 192, 192)
+            input_image = tf.cast(img, dtype=tf.float32)
+            # Setup input and output
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
+
+            # Make predictions
+            interpreter.set_tensor(input_details[0]['index'], np.array(input_image))
+            interpreter.invoke()
+            keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
+
+            # resize and render mask
+
+            frame = cv2.resize(frame, dsize=(videoWidth, videoHeight), interpolation=cv2.INTER_CUBIC)
+            mask = cv2.resize(mask, dsize=(videoWidth, videoHeight), interpolation=cv2.INTER_CUBIC)
+
+            # change dtype of mask
+            mask = mask.astype('uint8')
+
+            frame = np.concatenate((frame, np.full((videoHeight, videoWidth, 1), 255, dtype=np.uint8)), axis=2)
+            frame = cv2.addWeighted(frame, 1, mask, 0.5, 0)
+
+
+            y, x, c = frame.shape
+            shaped = np.squeeze(np.multiply(keypoints_with_scores, [y, x, 1]))
+            # collisions
+            for i in range(0, len(shaped)):
+                xpos = shaped[i][0]
+                ypos = shaped[i][1]
+                confidence = shaped[i][2]
+                if xpos < y and ypos < x and confidence > 0.4:
+                    if mask[int(xpos), int(ypos), 3] > 0:
+                        colors[i] = (0, 0, 0)
+                        collision = True;
+                    else:
+                        colors[i] = (255, 255, 255)
+            # Rendering
+            draw_connections(frame, keypoints_with_scores, EDGES, 0.4)
+            draw_keypoints(frame, shaped, 0.4, colors)
+
+            frame = cv2.flip(frame, 1)
+            #put the timer on the screen
+            frame = cv2.putText(frame, str(5 - int(finish - start)), (15, 50), \
+                                fontface, fontscale, fontcolor, thickness=3)
+            if collision:
+                # show collision circle
+                frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0, 0, 255), thickness=-1)
+            else:
+                frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0, 255, 0), thickness=-1)
+            cv2.imshow(name, frame)
+            input = cv2.waitKey(10)
+            if input == ord('q'):
+                end = True
+            if input == ord('s'):
+                skipEvent = True
+            if input == ord('c'):
+                cut = True
+            if input == ord('r'):
+                restart = True
+            # if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
+            #     end = True
+            finish = time.time()
+        skipEvent = False
+
+        #display a fail/success screen
+        start = time.time()
+        finish = time.time()
+        if collision:
+            fails += 1
+        else:
+            score += 1
+        rounds += 1
+        while finish - start < 3 and not end and not skipEvent and not cut and not restart:
+            if fails == 3:
+                frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 0, 255), thickness=-1)
+                frame = cv2.putText(frame, "Game Over", (int(videoWidth / 2) - 180,int(videoHeight / 2)), \
+                                    fontface, fontscale, fontcolor, thickness=3)
+                frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
+                                    fontface, fontscale, fontcolor, thickness=3)
+            elif collision:
+                frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 0,255), thickness = -1)
+                frame = cv2.putText(frame, "Fail", (int(videoWidth / 2) - 60,int(videoHeight / 2)), \
+                                    fontface, fontscale, fontcolor, thickness=3)
+                frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
+                                    fontface, fontscale, fontcolor, thickness=3)
+            else:
+                frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 255, 0), thickness=-1)
+                frame = cv2.putText(frame, "Pass", (int(videoWidth / 2) - 80,int(videoHeight / 2)), \
+                                    fontface, fontscale, fontcolor, thickness=3)
+                frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
+                                    fontface, fontscale, fontcolor, thickness=3)
+            cv2.imshow(name, frame)
+            finish = time.time()
+            input = cv2.waitKey(10)
+            if input == ord('q'):
+                end = True
+            if input == ord('s'):
+                skipEvent = True
+            if input == ord('c'):
+                cut = True
+            if input == ord('r'):
+                restart = True
+            # if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
+            #     end = True
+        
+        # Recycle paths if all images used
+        if len(paths) == 0:
+            while len(used) > 0:
+                paths.append(used.pop())
+            random.shuffle(paths)
+        
+    # end screen
+    while not end and not restart:
+        frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (255, 255, 255), thickness=-1)
+        frame = cv2.putText(frame, "Rounds Survived: " + str(rounds), (int(videoWidth / 2) - 300, int(videoHeight / 2) - 70), \
+                            fontface, \
+                            fontscale, fontcolor, thickness=3)
+        frame = cv2.putText(frame, "Score: " + str(score), (int(videoWidth / 2) - 150, int(videoHeight / 2) - 25), fontface, \
+                            fontscale, fontcolor, thickness=3)
+        frame = cv2.putText(frame, "Press q to quit", (int(videoWidth / 2) - 250, int(videoHeight / 2) + 25), fontface, \
+                            fontscale, fontcolor, thickness=3)
+        frame = cv2.putText(frame, "Press r to restart", (int(videoWidth / 2) - 300, int(videoHeight / 2) + 75), fontface, \
+                            fontscale, fontcolor, thickness=3)
+        cv2.imshow(name, frame)
+        input = cv2.waitKey(10)
+        if input == ord('c') or cv2.waitKey(10) & 0xFF == ord('q'):
             end = True
+        if input == ord('r'):
+            restart = True
+        # if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
+        #     end = True
     
-    # Recycle paths if all images used
-    if len(paths) == 0:
+    if restart:
         while len(used) > 0:
             paths.append(used.pop())
-        random.shuffle(paths)
-    
-# end screen
-while not end:
-    frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (255, 255, 255), thickness=-1)
-    frame = cv2.putText(frame, "Rounds Survived: " + str(rounds), (int(videoWidth / 2) - 300, int(videoHeight / 2) - 50), \
-                        fontface, \
-                        fontscale, fontcolor, thickness=3)
-    frame = cv2.putText(frame, "Score: " + str(score), (int(videoWidth / 2) - 150, int(videoHeight / 2)), fontface, \
-                        fontscale, fontcolor, thickness=3)
-    frame = cv2.putText(frame, "Press q to quit", (int(videoWidth / 2) - 250, int(videoHeight / 2) + 50), fontface, \
-                        fontscale, fontcolor, thickness=3)
-    cv2.imshow(name, frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        end = True
-    if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
-        end = True
+        paths.sort()
 
 cap.release()
 cv2.destroyAllWindows()
