bc2234e9a757084e9247d1aeeec83e0886309bd6
Steven Zhang <50186662+sbeven@users.noreply.github.com>
2022-04-09 16:28:37 -0400

    calibration + start screen + optimizations

--- MoveNet_Refined.py
+++ MoveNet_Refined.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # 0. Install and Import Dependencies
-
-# In[ ]:
 
 
 # pip install tensorflow==2.4.1 tensorflow-gpu==2.4.1 opencv-python matplotlib
 
-# In[ ]:
+
 
 
 import tensorflow as tf
@@ -57,35 +51,25 @@ def draw_keypoints(frame, shaped, confidence_threshold, colors):
     for i in range(0, len(shaped)):
         ky, kx, kp_conf = shaped[i]
         if kp_conf > confidence_threshold:
-            cv2.circle(frame, (int(kx), int(ky)), 4, colors[i], -1)
-            # # 4. Draw Edges
+            cv2.circle(frame, (int(kx), int(ky)), 10, colors[i], -1)
 
 
-# In[ ]:
-# In[ ]:
-
-
-# # 1. Load Model
-
-# In[ ]:
 
 
 interpreter = tf.lite.Interpreter(model_path='lite-model_movenet_singlepose_lightning_3.tflite')
 interpreter.allocate_tensors()
 
 
-# # 2. Make Detections
 
-# In[ ]:
-
+#initializations
 paths = ["Mask1.png",
         "Mask2.png",
         "Mask3.png",
         "Mask4.png",
         "Mask5.png"]
-
-videoWidth = 853
-videoHeight = 640
+name = "Hole in the Wall"
+videoWidth = 1000
+videoHeight = 750
 colors = [(255, 255, 255)] * 17
 cap = cv2.VideoCapture(0)
 collision = False
@@ -99,6 +83,103 @@ rounds = 0
 end = False
 skipEvent = False
 input
+
+#user calibration
+start = time.time()
+finish = time.time()
+skipEvent = False
+mask_path = paths[0] #replace with calibration mask
+mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
+while not end and not skipEvent:
+
+    collision = False
+    ret, frame = cap.read()
+
+    # Reshape image
+    img = frame.copy()
+    img = tf.image.resize_with_pad(np.expand_dims(img, axis=0), 192, 192)
+    input_image = tf.cast(img, dtype=tf.float32)
+    # Setup input and output
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Make predictions
+    interpreter.set_tensor(input_details[0]['index'], np.array(input_image))
+    interpreter.invoke()
+    keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
+
+    # resize and render mask
+
+    frame = cv2.resize(frame, dsize=(videoWidth, videoHeight), interpolation=cv2.INTER_CUBIC)
+    mask = cv2.resize(mask, dsize=(videoWidth, videoHeight), interpolation=cv2.INTER_CUBIC)
+
+    # change dtype of mask
+    mask = mask.astype('uint8')
+
+    frame = np.concatenate((frame, np.full((videoHeight, videoWidth, 1), 255, dtype=np.uint8)), axis=2)
+    frame = cv2.addWeighted(frame, 1, mask, 0.5, 0)
+
+    y, x, c = frame.shape
+    shaped = np.squeeze(np.multiply(keypoints_with_scores, [y, x, 1]))
+    # collisions
+    for i in range(0, len(shaped)):
+        xpos = shaped[i][0]
+        ypos = shaped[i][1]
+        confidence = shaped[i][2]
+        if xpos < y and ypos < x and confidence > 0.4:
+            if mask[int(xpos), int(ypos), 3] > 0:
+                colors[i] = (0, 0, 0)
+                collision = True;
+            else:
+                colors[i] = (255, 255, 255)
+    # Rendering
+    draw_connections(frame, keypoints_with_scores, EDGES, 0.4)
+    draw_keypoints(frame, shaped, 0.4, colors)
+
+    frame = cv2.flip(frame, 1)
+    # put the timer on the screen
+    if collision:
+        start = time.time()
+        frame = cv2.putText(frame, "Calibrate body", (15, 50), \
+                            fontface, fontscale, fontcolor, thickness=3)
+        # red dot to show that there's a collision
+        frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0,0,255), thickness=-1)
+    else:
+        frame = cv2.putText(frame, "Hold position: " + str(3 - int(finish - start)), (15, 50), \
+                        fontface, fontscale, fontcolor, thickness=3)
+        frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0, 255, 0), thickness=-1)
+    cv2.imshow(name, frame)
+    input = cv2.waitKey(10)
+    if input == ord('q'):
+        end = True
+    if input == ord('s'):
+        skipEvent = True
+    if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
+        end = True
+    finish = time.time()
+    if finish - start > 3:
+        skipEvent = True
+skipEvent = False
+# game start text
+start = time.time()
+finish = time.time()
+while finish - start < 3 and not end and not skipEvent:
+    frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (255, 255, 255), thickness=-1)
+    frame = cv2.putText(frame, "Game will start in " + \
+                        str(3 - int(finish - start)), (int(videoWidth / 2) - 330,int(videoHeight / 2)), \
+                        fontface, fontscale, fontcolor, thickness=3)
+    cv2.imshow(name, frame)
+    finish = time.time()
+    input = cv2.waitKey(10)
+    if input == ord('q'):
+        end = True
+    if input == ord('s'):
+        skipEvent = True
+    if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
+        end = True
+
+skipEvent = False
+# game start screen
 while fails < 3 and not end:
 
     start = time.time()
@@ -106,6 +187,8 @@ while fails < 3 and not end:
     skipEvent = False
     mask_path = paths[random.randint(0,len(paths) - 1)]
     mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
+
+    #game loop
     while finish - start < 5 and not end and not skipEvent:
         collision = False
         ret, frame = cap.read()
@@ -125,13 +208,13 @@ while fails < 3 and not end:
 
         # resize and render mask
 
-        frame = cv2.resize(frame, dsize=(843, 640), interpolation=cv2.INTER_CUBIC)
-        mask = cv2.resize(mask, dsize=(843, 640), interpolation=cv2.INTER_CUBIC)
+        frame = cv2.resize(frame, dsize=(videoWidth, videoHeight), interpolation=cv2.INTER_CUBIC)
+        mask = cv2.resize(mask, dsize=(videoWidth, videoHeight), interpolation=cv2.INTER_CUBIC)
 
         # change dtype of mask
         mask = mask.astype('uint8')
 
-        frame = np.concatenate((frame, np.full((640, 843, 1), 255, dtype=np.uint8)), axis=2)
+        frame = np.concatenate((frame, np.full((videoHeight, videoWidth, 1), 255, dtype=np.uint8)), axis=2)
         frame = cv2.addWeighted(frame, 1, mask, 0.5, 0)
 
 
@@ -144,7 +227,7 @@ while fails < 3 and not end:
             confidence = shaped[i][2]
             if xpos < y and ypos < x and confidence > 0.4:
                 if mask[int(xpos), int(ypos), 3] > 0:
-                    colors[i] = (0, 255, 0)
+                    colors[i] = (0, 0, 0)
                     collision = True;
                 else:
                     colors[i] = (255, 255, 255)
@@ -156,16 +239,22 @@ while fails < 3 and not end:
         #put the timer on the screen
         frame = cv2.putText(frame, str(5 - int(finish - start)), (15, 50), \
                             fontface, fontscale, fontcolor, thickness=3)
-        cv2.imshow('MoveNet Lightning', frame)
+        if collision:
+            # show collision circle
+            frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0, 0, 255), thickness=-1)
+        else:
+            frame = cv2.circle(frame, (videoWidth - 25, 25), 20, (0, 255, 0), thickness=-1)
+        cv2.imshow(name, frame)
         input = cv2.waitKey(10)
         if input == ord('q'):
             end = True
         if input == ord('s'):
             skipEvent = True
-        if cv2.getWindowProperty('MoveNet Lightning', cv2.WND_PROP_VISIBLE) < 1:
+        if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
             end = True
         finish = time.time()
     skipEvent = False
+
     #display a fail/success screen
     start = time.time()
     finish = time.time()
@@ -175,58 +264,51 @@ while fails < 3 and not end:
         score += 1
     rounds += 1
     while finish - start < 3 and not end and not skipEvent:
-        if collision:
-            frame = cv2.rectangle(frame, (0, 0), (843, 640), (0, 0,255), thickness = -1)
-            frame = cv2.putText(frame, "Fail", (350, 340), fontface, fontscale, fontcolor, thickness=3)
+        if fails == 3:
+            frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 0, 255), thickness=-1)
+            frame = cv2.putText(frame, "Game Over", (int(videoWidth / 2) - 180,int(videoHeight / 2)), \
+                                fontface, fontscale, fontcolor, thickness=3)
+            frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
+                                fontface, fontscale, fontcolor, thickness=3)
+        elif collision:
+            frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 0,255), thickness = -1)
+            frame = cv2.putText(frame, "Fail", (int(videoWidth / 2) - 60,int(videoHeight / 2)), \
+                                fontface, fontscale, fontcolor, thickness=3)
             frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
                                 fontface, fontscale, fontcolor, thickness=3)
         else:
-            frame = cv2.rectangle(frame, (0, 0), (843, 640), (0, 255, 0), thickness=-1)
-            frame = cv2.putText(frame, "Pass", (350, 340), fontface, fontscale, fontcolor, thickness=3)
+            frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (0, 255, 0), thickness=-1)
+            frame = cv2.putText(frame, "Pass", (int(videoWidth / 2) - 80,int(videoHeight / 2)), \
+                                fontface, fontscale, fontcolor, thickness=3)
             frame = cv2.putText(frame, str(3 - int(finish - start)), (15, 50), \
                                 fontface, fontscale, fontcolor, thickness=3)
-        cv2.imshow('MoveNet Lightning', frame)
+        cv2.imshow(name, frame)
         finish = time.time()
         input = cv2.waitKey(10)
         if input == ord('q'):
             end = True
         if input == ord('s'):
             skipEvent = True
-        if cv2.getWindowProperty('MoveNet Lightning', cv2.WND_PROP_VISIBLE) < 1:
+        if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
             end = True
 # end screen
 while not end:
-    frame = cv2.rectangle(frame, (0, 0), (843, 640), (255, 255, 255), thickness=-1)
-    frame = cv2.putText(frame, "Rounds Survived: " + str(rounds), (140, 250), fontface, \
-                        fontscale, (0,0,0), thickness=3)
-    frame = cv2.putText(frame, "Score: " + str(score), (140, 300), fontface, \
-                        fontscale, (0,0,0), thickness=3)
-    frame = cv2.putText(frame, "Press q to quit", (140, 350), fontface, \
-                        fontscale, (0,0,0), thickness=3)
-    cv2.imshow('MoveNet Lightning', frame)
+    frame = cv2.rectangle(frame, (0, 0), (videoWidth, videoHeight), (255, 255, 255), thickness=-1)
+    frame = cv2.putText(frame, "Rounds Survived: " + str(rounds), (int(videoWidth / 2) - 300, int(videoHeight / 2) - 50), \
+                        fontface, \
+                        fontscale, fontcolor, thickness=3)
+    frame = cv2.putText(frame, "Score: " + str(score), (int(videoWidth / 2) - 150, int(videoHeight / 2)), fontface, \
+                        fontscale, fontcolor, thickness=3)
+    frame = cv2.putText(frame, "Press q to quit", (int(videoWidth / 2) - 250, int(videoHeight / 2) + 50), fontface, \
+                        fontscale, fontcolor, thickness=3)
+    cv2.imshow(name, frame)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         end = True
-    if cv2.getWindowProperty('MoveNet Lightning', cv2.WND_PROP_VISIBLE) < 1:
+    if cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1:
         end = True
-
-
-cv2.imshow('MoveNet Lightning', frame)
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-# # 3. Draw Keypoints
-
-# In[ ]:
-
-
-
-
-
-
-# In[ ]:
-
 
 
 
